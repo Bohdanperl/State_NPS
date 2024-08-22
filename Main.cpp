076e1e68dcae8996349e7d1963d133b2d8453d7c
Bohdan.perliei <88403935+Bohdanperl@users.noreply.github.com>
2024-08-22 15:15:06 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,26 +4,29 @@
 
 int main()
 {
-	std::cout << "Hi, this is NPC game, let`s start =) \n";
-	int count_Npc{};
-	std::cout << "Enter, how many NPC you want to create?\n";
-	std::cin >> count_Npc;
-	if (count_Npc <= 0)
-	{
-		std::cout << "ERROR!!!\n";
-		return 0;
-	}
-	Game game{count_Npc};
-	game.run_NPCs();
-	std::thread th{[&game]()
-								 {
-									 while (!game.game_end())
-									 {
-										 std::this_thread::sleep_for(std::chrono::milliseconds(200));
-									 }
-								 }};
-	th.join();
-	std::cout << "\n";
-	std::cout << "NPC game is over )= \n";
-	return 0;
+    std::cout << "Hi, this is NPC game, let`s start =) \n";
+    int count_Npc{};
+    std::cout << "Enter, how many NPC you want to create?\n";
+    std::cin >> count_Npc;
+    
+    if (count_Npc <= 0)
+    {
+        std::cout << "ERROR!!!\n";
+        return 0;
+    }
+    
+    Game game{count_Npc};
+    game.run_NPCs();
+    
+    std::thread th{[&game]() {
+        while (!game.game_end())
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        }
+    }};
+    
+    th.join();
+    std::cout << "\n";
+    std::cout << "NPC game is over )= \n";
+    return 0;
 }
